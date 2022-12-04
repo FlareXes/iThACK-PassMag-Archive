908ef048e16aebdc11f05af0496242fec56043ff
FlareXes <70066527+FlareXes@users.noreply.github.com>
2022-12-04 09:26:10 +0530

    utility functions

--- iThACK/security/utils.py
+++ iThACK/security/utils.py
@@ -12,3 +12,17 @@ def kdf_scrypt(key, random_bytes):
     parallel_factor = 1
     key_length = 32  # bytes
     return scrypt(password=key, salt=random_bytes, key_len=key_length, N=cost_factor, r=rounds, p=parallel_factor)
+
+
+def generate_master_password() -> bytes:
+    return kdf_scrypt(input("Master Key: "), salt(32))
+
+
+def get_master_password() -> bytes:
+    return kdf_scrypt(input("Master Key: "), salt(32))
+
+
+def get_mp_hash() -> bytes:
+    # Simply hashing master password is insecure so, I added some cost factor.
+    static_salt = b"\xb5\x14\xdbKV\xa5Q&\xc5\xef\xfa\x87@]\xa6\xcf?\x8dAn\x97a\x11\xa2\xf0\xae\xa4r\xd7\x0b\xeaY"
+    return kdf_scrypt(input("Master Key: "), static_salt)
