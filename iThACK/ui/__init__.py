873a582751a7b6e9eb238c11d33d3563394472c5
FlareXes <70066527+FlareXes@users.noreply.github.com>
2022-12-12 20:52:04 +0530

    improve ui codebase

---
+++ iThACK/ui/__init__.py
@@ -0,0 +1,45 @@
+from typing import Tuple
+
+from rich.console import Console
+from rich.table import Table
+
+
+class _Table:
+    def __init__(self) -> None:
+        self.table = Table()
+        self.table.add_column("S.No.", justify="center", style="magenta", header_style="magenta")
+        self.table.add_column("Site", justify="center", style="green", header_style="green")
+        self.table.add_column("Username / E-Mail", justify="center", style="red", header_style="red")
+        self.table.add_column("Url", justify="center", style="green", header_style="green")
+
+    def add_row(self, account: Tuple):
+        self.table.add_row(*account)
+
+    def show_table(self):
+        console = Console()
+        console.print(self.table)
+
+
+class Print:
+    @staticmethod
+    def success(text):
+        console = Console()
+        console.print(f"[bold green]{text}[/bold green]")
+
+    @staticmethod
+    def warning(text):
+        console = Console()
+        console.print(f"[bold yellow]{text}[/bold yellow]")
+
+    @staticmethod
+    def fail(text):
+        console = Console()
+        console.print(f"[bold red]{text}[/bold red]")
+
+
+def tabulate(accounts):
+    t = _Table()
+    for account in accounts:
+        account = tuple(map(str, account))
+        t.add_row(account)
+    t.show_table()
