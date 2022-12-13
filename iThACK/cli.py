d6bdf8ae8b5aea06eeda2d27a3b1bd086a7aacdd
FlareXes <70066527+FlareXes@users.noreply.github.com>
2022-12-13 13:54:00 +0530

    add basic functionality

--- iThACK/cli.py
+++ iThACK/cli.py
@@ -1,6 +1,4 @@
-import sys
-
-from iThACK.manage import select_account, add_account
+from iThACK.manage import select_account, add_account, view_password, delete_account
 from iThACK.security import login
 from iThACK.ui import Print
 
@@ -13,4 +11,11 @@ def process(argv, argc):
 
     if argv[0] == "add":
         add_account(mp_hash)
-        Print.success(":) Added new account")
+
+    elif argv[0] == "view":
+        _id = select_account()
+        view_password(_id, mp_hash)
+
+    elif argv[0] == "remove":
+        _id = select_account()
+        delete_account(_id)
--- iThACK/manage.py
+++ iThACK/manage.py
@@ -1,14 +1,13 @@
 from iThACK.database.database import Database, Filter
-from iThACK.security import encrypt_password
+from iThACK.security import encrypt_password, AES256
 from iThACK.template import Account
-from iThACK.ui import tabulate
+from iThACK.ui import tabulate, Print, Input
 
 
 def select_account():
     tabulate(Database().read)
     _id = int(input(": "))
-    account = Filter().select(_id)
-    return account
+    return _id
 
 
 def add_account(mp_hash: bytes):
@@ -19,3 +18,18 @@ def add_account(mp_hash: bytes):
 
     account = Account(0, site, username, url)
     Database(account, creds).create()
+    Print.success(":) Added new account")
+
+
+def delete_account(_id):
+    account, cc = Filter().select(_id)
+
+    if Input.confirm():
+        Database(account=account).delete()
+        Print.fail("[-] Account Removed")
+
+
+def view_password(_id, mp_hash):
+    account, cc = Filter().select(_id)
+    aes = AES256(mp_hash)
+    print(aes.decrypt(cc))
