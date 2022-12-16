29df387144038a0cf36b580a3c35aac088ed2d19
FlareXes <70066527+FlareXes@users.noreply.github.com>
2022-12-16 06:20:11 +0530

    switch to dataclasses

--- iThACK/template.py
+++ iThACK/template.py
@@ -1,14 +1,17 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class Account:
-    def __init__(self, acc_id: int, site: str, username: str, url: str):
-        self.acc_id = acc_id
-        self.site = site
-        self.username = username
-        self.url = url
+    acc_id: int
+    site: str
+    username: str
+    url: str
 
 
+@dataclass
 class CipherConfig:
-    def __init__(self, ciphertext: bytes, salt: bytes, tag: bytes, nonce: bytes):
-        self.ciphertext = ciphertext
-        self.salt = salt
-        self.tag = tag
-        self.nonce = nonce
+    ciphertext: bytes
+    salt: bytes
+    tag: bytes
+    nonce: bytes
